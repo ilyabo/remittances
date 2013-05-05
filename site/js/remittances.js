0a9b84445969e329bed7c42f0034c6989e8aafdd
Ilya Boyandin <ilyabo@gmail.com>
2013-05-05 23:18:06 +0200

    using N/A instead of ... for missing numbers

--- site/js/remittances.js
+++ site/js/remittances.js
@@ -68,7 +68,7 @@ var migrationsByOriginCode = {};
 var numberFormat = (function() {
   var fmt = d3.format(",.0f");
   return function(v) {
-    if (v == null  ||  isNaN(v)) return "...";
+    if (v == null  ||  isNaN(v)) return msg("amount.not-available");
     if (v >= 1e9) return msg("amount.billions",  fmt(v / 1e9));
     if (v >= 1e6) return msg("amount.millions",  fmt(v / 1e6));
 //    if (v >= 1e3) return msg("amount.thousands", fmt(v / 1e3));
@@ -77,7 +77,7 @@ var numberFormat = (function() {
 })();
 
 var moneyFormat = function(v) {
-  if (v == null  ||  isNaN(v)) return "...";
+  if (v == null  ||  isNaN(v)) return msg("amount.not-available");
   return msg("money", numberFormat(v));
 };
 
